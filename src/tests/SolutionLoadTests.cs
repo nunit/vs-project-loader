856b8d580b2551a1df2174d563ac0525661bbaf2
Charlie Poole <charliepoole@gmail.com>
2021-09-19 08:05:37 -0700

    Simplify resource names

--- src/tests/SolutionLoadTests.cs
+++ src/tests/SolutionLoadTests.cs
@@ -34,10 +34,10 @@ namespace NUnit.Engine.Services.ProjectLoaders.Tests
         [Test]
         public void VS2003Solution()
         {
-            using (new TestResource("legacy-csharp-sample.csproj", NormalizePath(@"csharp\legacy-csharp-sample.csproj")))
-            using (new TestResource("legacy-jsharp-sample.vjsproj", NormalizePath(@"jsharp\legacy-jsharp-sample.vjsproj")))
-            using (new TestResource("legacy-vb-sample.vbproj", NormalizePath(@"vb\legacy-vb-sample.vbproj")))
-            using (new TestResource("legacy-cpp-sample.vcproj", NormalizePath(@"cpp-sample\legacy-cpp-sample.vcproj")))
+            using (new TestResource("legacy-sample.csproj", NormalizePath(@"csharp\legacy-sample.csproj")))
+            using (new TestResource("legacy-sample.vjsproj", NormalizePath(@"jsharp\legacy-sample.vjsproj")))
+            using (new TestResource("legacy-sample.vbproj", NormalizePath(@"vb\legacy-sample.vbproj")))
+            using (new TestResource("legacy-sample.vcproj", NormalizePath(@"cpp-sample\legacy-sample.vcproj")))
             using (TestResource file = new TestResource("legacy-samples.sln"))
             {
                 IProject project = _loader.LoadFrom(file.Path);
@@ -51,10 +51,10 @@ namespace NUnit.Engine.Services.ProjectLoaders.Tests
         [Test]
         public void VS2005Solution()
         {
-            using (new TestResource("nonsdk-csharp-sample.csproj", NormalizePath(@"csharp\nonsdk-csharp-sample.csproj")))
-            using (new TestResource("nonsdk-jsharp-sample.vjsproj", NormalizePath(@"jsharp\nonsdk-jsharp-sample.vjsproj")))
-            using (new TestResource("nonsdk-vb-sample.vbproj", NormalizePath(@"vb\nonsdk-vb-sample.vbproj")))
-            using (new TestResource("legacy-cpp-sample.vcproj", NormalizePath(@"cpp-sample\legacy-cpp-sample.vcproj")))
+            using (new TestResource("nonsdk-sample.csproj", NormalizePath(@"csharp\nonsdk-sample.csproj")))
+            using (new TestResource("nonsdk-sample.vjsproj", NormalizePath(@"jsharp\nonsdk-sample.vjsproj")))
+            using (new TestResource("nonsdk-sample.vbproj", NormalizePath(@"vb\nonsdk-sample.vbproj")))
+            using (new TestResource("legacy-sample.vcproj", NormalizePath(@"cpp-sample\legacy-sample.vcproj")))
             using (TestResource file = new TestResource("solution-vs2005-samples.sln"))
             {
                 IProject project = _loader.LoadFrom(file.Path);
@@ -92,7 +92,7 @@ namespace NUnit.Engine.Services.ProjectLoaders.Tests
         [Test]
         public void SolutionWithWebApplication()
         {
-            using (new TestResource("legacy-csharp-sample.csproj", NormalizePath(@"legacy-csharp-sample\legacy-csharp-sample.csproj")))
+            using (new TestResource("legacy-sample.csproj", NormalizePath(@"legacy-sample\legacy-sample.csproj")))
             using (TestResource file = new TestResource("solution-with-web-application.sln"))
             {
                 IProject project = _loader.LoadFrom(file.Path);
@@ -105,8 +105,8 @@ namespace NUnit.Engine.Services.ProjectLoaders.Tests
         [Test]
         public void SolutionWithUnmanagedCpp()
         {
-            using (new TestResource("legacy-csharp-sample.csproj", NormalizePath(@"legacy-csharp-sample\legacy-csharp-sample.csproj")))
-            using (new TestResource("legacy-cpp-unmanaged.vcproj", NormalizePath(@"legacy-cpp-unmanaged\legacy-cpp-unmanaged.vcproj")))
+            using (new TestResource("legacy-sample.csproj", NormalizePath(@"legacy-sample\legacy-sample.csproj")))
+            using (new TestResource("legacy-unmanaged.vcproj", NormalizePath(@"legacy-unmanaged\legacy-unmanaged.vcproj")))
             using (TestResource file = new TestResource("solution-with-unmanaged-cpp.sln"))
             {
                 IProject project = _loader.LoadFrom(file.Path);
@@ -120,8 +120,8 @@ namespace NUnit.Engine.Services.ProjectLoaders.Tests
         [Test]
         public void SolutionWithDisabledProject()
         {
-            using (new TestResource("nonsdk-csharp-sample.csproj", NormalizePath(@"csharp-sample\nonsdk-csharp-sample.csproj")))
-            using (new TestResource("nonsdk-csharp-debug-only.csproj", NormalizePath(@"csharp-debug-only\nonsdk-csharp-debug-only.csproj")))
+            using (new TestResource("nonsdk-sample.csproj", NormalizePath(@"csharp-sample\nonsdk-sample.csproj")))
+            using (new TestResource("nonsdk-debug-only.csproj", NormalizePath(@"csharp-debug-only\nonsdk-debug-only.csproj")))
             using (TestResource file = new TestResource("solution-with-disabled-project.sln"))
             {
                 IProject project = _loader.LoadFrom(file.Path);
@@ -134,8 +134,8 @@ namespace NUnit.Engine.Services.ProjectLoaders.Tests
         [Test]
         public void SolutionWithNonNunitTestProject()
         {
-            using (new TestResource("nonsdk-csharp-sample.csproj", NormalizePath(@"csharp-sample\nonsdk-csharp-sample.csproj")))
-            using (new TestResource("nonsdk-csharp-debug-only-no-nunit.csproj", NormalizePath(@"nonsdk-csharp-debug-only-no-nunit\csharp-debug-only-no-nunit.csproj")))
+            using (new TestResource("nonsdk-sample.csproj", NormalizePath(@"csharp-sample\nonsdk-sample.csproj")))
+            using (new TestResource("nonsdk-debug-only-no-nunit.csproj", NormalizePath(@"debug-only-no-nunit\nonsdk-debug-only-no-nunit.csproj")))
             using (TestResource file = new TestResource("solution-with-non-nunit-project.sln"))
             {
                 IProject project = _loader.LoadFrom(file.Path);
@@ -148,7 +148,7 @@ namespace NUnit.Engine.Services.ProjectLoaders.Tests
         [Test]
         public void SolutionWithProjectUsingPackageReference()
         {
-            using (new TestResource("nonsdk-csharp-with-package-reference.csproj", NormalizePath(@"project-with-package-reference\nonsdk-csharp-with-package-reference.csproj")))
+            using (new TestResource("nonsdk-package-reference.csproj", NormalizePath(@"project-with-package-reference\nonsdk-package-reference.csproj")))
             using (TestResource file = new TestResource("solution-with-package-reference.sln"))
             {
                 IProject project = _loader.LoadFrom(file.Path);
